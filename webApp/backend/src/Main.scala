81490f49e224a6ad99d360b854d826204ea7fc18
Vitalii Lysov <Vitalii.lysov@unzer.com>
2021-04-25 19:44:19 +0200

    fix: html rendering

--- webApp/backend/src/Main.scala
+++ webApp/backend/src/Main.scala
@@ -6,22 +6,32 @@ object Main extends cask.MainRoutes {
 
   @cask.get("/")
   def hello() = cask.Response(
-   "<!doctype html>" + "\n" + html(lang := "en",
-      head(
-        tag("title")("Hello from ScalaJS"),
-        script(src := "/files/webApp/frontend/fastOpt/dest/out.js")
-      ),
-      body(
-        h1("Hello :)"),
-        button(onclick := "clickAction()")("click me")
+    headers = Seq(
+      "Content-Type" -> "text/html",
+      "Server" -> "cask"
+    ),
+    data = doctype("html")(
+      html(
+        lang := "en",
+        head(
+          tag("title")("Hello from ScalaJS"),
+          script(src := "/files/webApp/frontend/fastOpt/dest/out.js")
+        ),
+        body(
+          h1("Hello :)"),
+          button(onclick := "clickAction()")("click me")
+        )
       )
-    ).render,
+    )
   )
 
   @cask.staticFiles("/files/")
-  def staticFiles() = "out/" // serve files from ./out/ in the root of the project 
+  def staticFiles() =
+    "out/" // serve files from ./out/ in the root of the project
 
-  @cask.staticResources("/static/") // unused, just documented as it was not very clear in docs
+  @cask.staticResources(
+    "/static/"
+  ) // unused, just documented as it was not very clear in docs
   def staticResources() = "." // serve files from resouces in this module
 
   initialize()
